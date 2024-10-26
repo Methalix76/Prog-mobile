0f93d563c7e7586e8655c3e47e5342522c4f00ef
Renzo Aedo <renzo.aedo@gmail.com>
2024-10-25 22:46:05 -0300

    Rev V251024.2245

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'cl.duoc.testfirebasetellevo',
   appName: 'tellevo',
   webDir: 'www'
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import {initializeApp} from '@firebase/app'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+
+async ngOnInit(){
+//verermos que no estemos en un celular
+  if(!Capacitor.isNativePlatform()){
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyDPG9puasY2EQWNzz-70pLRwWRkpaY5D5Q",
+      authDomain: "testfirebasetellevo.firebaseapp.com",
+      projectId: "testfirebasetellevo",
+      storageBucket: "testfirebasetellevo.appspot.com",
+      messagingSenderId: "155295787478",
+      appId: "1:155295787478:web:bffb550aaebbe725cadc77"
+    };
+
+    initializeApp(firebaseConfig)    
+}
+
+
+}
+
+
+
+
+
+
 }
