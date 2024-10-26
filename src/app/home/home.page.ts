fbec89333723246baa70e13eebdfc51dc4b79d61
Renzo Aedo <renzo.aedo@gmail.com>
2024-10-26 19:03:24 -0300

    Rev V251026.1903

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-import { ViewDidLeave } from '@ionic/angular';
+import { Component, inject } from '@angular/core';
+import { NavController, ViewDidEnter, ViewDidLeave, ViewWillEnter } from '@ionic/angular';
+import { LoginService } from '../login.service';
+import { FirebaseAuthentication } from '@capacitor-firebase/authentication';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  titulo:string = 'Te LLevo';
+export class HomePage implements ViewDidEnter,ViewWillEnter {
 
-  constructor() {}
+  usuario:string = '';
+  pass:string = '';
+  
+  navController = inject(NavController);
+  loginSrv = inject(LoginService);
+  constructor() {
+  }
 
+  //ocurre cuando se va a entrar a esta vista, antes de la animación
+  ionViewWillEnter(): void {
+    
+  }
+
+  //se ejecuta cuando ya se entró a la vista, despues de las animaciones
+  async ionViewDidEnter() {
+    this.usuario = '';
+    this.pass = '';
+  }
+  
+  async ingresar(){
+    
+    await this.loginSrv.login(this.usuario,this.pass);
+    this.navController.navigateForward('/bienvenida');
+    console.log('Ingresando');
+  }
 }
---
+++ src/app/utils/guards.ts
@@ -0,0 +1,25 @@
+import { inject } from "@angular/core"
+import { LoginService } from "../login.service";
+import { NavController } from "@ionic/angular";
+
+export const authGuard = async () =>{
+    const login = inject(LoginService);
+    const nav = inject(NavController);
+
+    await login.firebaseCargado;
+    if(!login.logeado){
+        nav.navigateRoot('/',{replaceUrl:true});  
+    }
+    return login.logeado;
+}
+
+export const guestGuard = async () =>{
+    const login = inject(LoginService);
+    const nav = inject(NavController);
+    await login.firebaseCargado;
+
+    if(login.logeado){
+        nav.navigateRoot('/bienvenida',{replaceUrl:true});
+    }
+    return !login.logeado;
+}
